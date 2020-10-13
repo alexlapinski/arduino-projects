69063fd117098ac75c3d153e440fe901167665b4
Alex Lapinski <contact@alexlapinski.name>
2020-10-13 19:28:21 -0400

    Completed ultrasonicreader component

--- ultrasonic-reader-arduino/ultrasonic-reader-arduino.ino
+++ ultrasonic-reader-arduino/ultrasonic-reader-arduino.ino
@@ -1,35 +1,31 @@
+const int trigPin = 10;
+const int echoPin = 9;
+
+long duration;
+int distance;
+
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-}
-
-void ledOn() {
-  digitalWrite(LED_BUILTIN, HIGH);
-}
-
-void ledOff() {
-  digitalWrite(LED_BUILTIN, LOW);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600); // maybe?
 }
 
 void loop() {
-  ledOff();
-  delay(100);                    
+  // Clear Trigger Pin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  // Set Trigger Pin on High State for 10 micro secondss
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Read the echoPin, return sound wave traveltime in micro seconds
+  duration = pulseIn(echoPin, HIGH);
   
-  ledOn();
-  delay(100);
-  
-  ledOff();
-  delay(100);
+  // Calculate the distace (using soundwave constants)
+  distance = duration * 0.034 / 2;
 
-  ledOn();
-  delay(100);
-
-  ledOff();
-  delay(100);
-
-  ledOn();
-  delay(100);
-
-  ledOff();
-  delay(5000);
-  
+  Serial.print("Distance: ");
+  Serial.println(distance);
 }
